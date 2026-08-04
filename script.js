7e451b85365a1898ed2bf23b48bad8fe6b9487d1
Mahmuda Akter <mahmudapakter@gmail.com>
2026-08-04 11:08:20 +0600

    revisions going on

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("Hello, World!");
+
+let day;
+let date = new Date().getDay();
+switch(date) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+
+console.log("Today is " + day);
+
+let text;
+let date2 = new Date().getDay();
+switch (date2) {
+  case 6:
+    text = "Today is Saturday";
+    break;
+  case 0:
+    text = "Today is Sunday";
+    break;
+  default:
+    text = "Looking forward to the Weekend";
+}
+
+console.log(text);
